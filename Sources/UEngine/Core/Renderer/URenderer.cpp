60d1a532c41c95e5c8bbc5902a744c4815c767c7
Alexey Antropov <alexey.antropov@artplant.no>
2013-09-10 23:54:04 +0400

    GL_REPEAT FOR TEXTURES

--- Sources/UEngine/Core/Renderer/URenderer.cpp
+++ Sources/UEngine/Core/Renderer/URenderer.cpp
@@ -125,8 +125,8 @@ int URenderer:: CreateTexture(UTexture *tex)
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 
     if(tex->GetTextureResId() != 0)
     {
