0d9fd90bec46a1b8a788da75cada1a4a55c8c1e1
Alexey Antropov <alexey.antropov@artplant.no>
2013-09-10 22:52:59 +0400

    art improve

--- Sources/UEngineStarter/UEngineStarter.cpp
+++ Sources/UEngineStarter/UEngineStarter.cpp
@@ -18,17 +18,17 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
     ULight *light1 = new ULight(&e.rf, vec4(10.0f, 10.0f, 0.0f, 0.0f));
     ULight *light2 = new ULight(&e.rf, vec4(1.0f, 1.0f, 1.0f, 0.0f));
     ULight *light3 = new ULight(&e.rf, vec4(10.0f, 7.0f, 0.0f, 0.0f));
-    ULight *light4 = new ULight(&e.rf, vec4(-12.0f, 9.0f, 10.0f, 0.0f));
+    ULight *light4 = new ULight(&e.rf, vec4(-12.0f, 1.0f, 10.0f, 0.0f));
     ULight *light5 = new ULight(&e.rf, vec4(-5.0f, 5.0f, 5.0f, 0.0f));
     ULight *light6 = new ULight(&e.rf, vec4(12.0f, 9.0f, 10.0f, 0.0f));
     ULight *light7 = new ULight(&e.rf, vec4(-5.0f, 4.0f, 5.0f, 0.0f));
-    ULight *light8 = new ULight(&e.rf, vec4(-2.0f, 30.0f, 1.0f, 0.0f));
+    ULight *light8 = new ULight(&e.rf, vec4(-2.0f, 8.0f, 1.0f, 0.0f));
     
     UScript *script = dynamic_cast<UScript*>(e.rf.Create(URESOURCE_SCRIPT));
     script->Load("data\\Scripts\\test_script.xml");
 
     light8->AddComponent((UComponent*)script);
-    light8->SetAttenuation(vec3(0.0f, 0.02f, 0.0f));
+    light8->SetAttenuation(vec3(0.0f, 0.06f, 0.0f));
     light8->SetSpotCosCutoff(60.1f);
     light8->SetSpotExponent(20.0f);
     light8->SetSpotDirection(vec4(0.0f, -1.0f, 0.0f, 1.0f));
